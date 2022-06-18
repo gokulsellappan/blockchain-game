179a9eddd880fb83398a57ecb1a1552ed36b85ff
gokulsellappan <gokulsellappan@gmail.com>
2022-06-18 22:49:00 +0530

    token-collecting-game

--- src/components/App.js
+++ src/components/App.js
@@ -184,7 +184,6 @@ chooseImage =(cardId)=>{
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
             target="_blank"
             rel="noopener noreferrer"
           >
