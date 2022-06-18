f6554752db083ea65a676c2d97d7854a4285a38b
gokulsellappan <gokulsellappan@gmail.com>
2022-06-18 22:35:32 +0530

    Token collecting Game

--- src/contracts/MemoryToken.sol
+++ src/contracts/MemoryToken.sol
@@ -3,6 +3,16 @@ pragma solidity ^0.5.0;
 import "./ERC721Full.sol";
 
 contract MemoryToken is ERC721Full {
-  // Code goes here...
+ 
+  constructor()ERC721Full("MemoryToken","MEMORY") public { 
+         
+  }
+  function mint(address _to,string memory _tokenURI) public returns(bool){
+        uint tokenId=totalSupply().add(1);
+        _mint(_to,tokenId);
+        _setTokenURI(tokenId,_tokenURI);
+        return true;
+      }
+
 
 }
--- test/MemoryToken.test.js
+++ test/MemoryToken.test.js
@@ -5,5 +5,68 @@ require('chai')
   .should()
 
 contract('Memory Token', (accounts) => {
-  // code goes here...
+  let token
+
+  before(async()=>{
+    token=await MemoryToken.deployed()
+  })
+
+  describe("deploy token",async()=>{
+    it('deployee successfully',async()=>{
+      const address=token.address
+      assert.notEqual(address, 0x0)
+      assert.notEqual(address, '')
+      assert.notEqual(address, null)
+      assert.notEqual(address, undefined)
+
+    })
+
+    it('its has a name',async()=>{
+      const name= await token.name()
+      assert.equal(name,'MemoryToken')
+    })
+     it('its has a symbol',async()=>{
+      const symbol= await token.symbol()
+      assert.equal(symbol,'MEMORY')
+    })
+  })
+
+  describe("mint token",async()=>{
+    let result
+    it('mint and setURI check',async()=>{
+      //mint operation check
+      result=await token.mint(accounts[0],'www.token-uri.com/nft')
+
+      //total supply check
+      result=await token.totalSupply()
+      assert.equal(result,'1','totalSupply is correct')
+
+      //balance of check
+      result=await token.balanceOf(accounts[0])
+      assert.equal(result,'1','balanceOf is correct')
+
+      //token belong to the owner
+      result=await token.ownerOf('1')
+      assert.equal(result.toString(),accounts[0].toString(),'token owned by owner')
+      result=await token.tokenOfOwnerByIndex(accounts[0],0)
+
+      //every token can see by owner
+      let tokenId=[]
+      let balanceof=await token.balanceOf(accounts[0])
+      for(let i=0;i<balanceof;i++){
+        let id=await token.tokenOfOwnerByIndex(accounts[0],i)
+        tokenId.push(id.toString())
+      }
+      let expected=['1']
+      assert.equal(tokenId.toString(),expected.toString(),'token are correct')
+
+      let tokenURI=await token.tokenURI('1')
+      assert.equal(tokenURI,'www.token-uri.com/nft')
+
+
+    })
+
+  })
+
+
 })
